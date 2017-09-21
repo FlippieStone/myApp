1e5189c9c9634c4da70543517d05a613ac10106b
Flippie Stone <atliamgdotmoc1@gmail.com>
2017-09-21 13:25:10 +0200

    changed

--- myApplication/ViewController.swift
+++ myApplication/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 5 {
+            coolLabel.text = "Button tapped 5 times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
